66b7a6a7595d3c8e1199478600424974498ce395
Luca <lucablazevic@yahoo.de>
2022-01-30 05:05:38 +0100

    add computer challenger

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,24 +1,28 @@
 module TicTacToe
-  BOARD_ROW_LINE = '-------'
+  BOARD_ROW_LINE = ' -------'
   BOARD_ROW = ['|' ,' ','|',' ', '|',' ', '|']
   BOARD_COLUMN = ['|','|','|','|','|','|','|']
-  DIAGONAL = [[1, 1], [3, 3], [5, 5]]
-  DIAGONAL2 = [[1, 5], [3, 3], [5, 1]]
+  DIAGONAL = [[2, 2], [4, 4], [6, 6]]
+  DIAGONAL2 = [[2, 6], [4, 4], [6, 2]]
   class Game
     attr_reader :playable_fields
 
     def initialize
-      @board = Array.new(7)
+      @board = Array.new(8) # 7
       start_game
     end
 
     def create_board
+      row_index = 1
       @board.each_index do |row|
-        if row.even?
+        if row.zero?
+          @board[row] = [' ', ' ', '1', ' ', '2', ' ', '3']
+        elsif row.odd?
           @board[row] = BOARD_ROW_LINE
-          @playable_fields = [[1, 1], [1, 3], [1, 5], [3, 1], [3, 3], [3, 5], [5, 1], [5, 3], [5, 5]]
+          @playable_fields = [[2, 2], [2, 4], [2, 6], [4, 2], [4, 4], [4, 6], [6, 2], [6, 4], [6, 6]]
         else
-          @board[row] = ['|', ' ', '|', ' ', '|', ' ', '|']
+          @board[row] = [row_index, '|', ' ', '|', ' ', '|', ' ', '|']
+          row_index += 1
         end
       end
     end
@@ -31,6 +35,7 @@ module TicTacToe
           puts @board[index]
         end
       end
+      puts ''
     end
 
     def playable_field?(field)
@@ -47,22 +52,18 @@ module TicTacToe
         @playable_fields.delete(position)
         player.fields.push(position)
       else
-        field_error
-        play_round(player)
+        GameError.field_error
+        player.play_round(self)
       end
     end
 
-    def field_error
-      puts 'field is not playable, try again.'
-    end
-
     def gameover?(player)
-      true if three_in_a_column?(player) || three_in_a_row?(player) || three_in_a_diagonal?(player)
+      true if three_in_a_column?(player) || three_in_a_row?(player) || three_in_a_diagonal?(player) || board_full?
     end
 
     def three_in_a_column?(player, summand = 0)
       count = 0
-      column = [[1, 1 + summand], [3, 1 + summand], [5, 1 + summand]]
+      column = [[2, 2 + summand], [4, 2 + summand], [6, 2 + summand]]
       column.each do |field|
         count += 1 if player.fields.include?(field)
       end
@@ -74,7 +75,7 @@ module TicTacToe
 
     def three_in_a_row?(player, summand = 0)
       count = 0
-      row = [[1 + summand, 1], [1 + summand, 3], [1 + summand, 5]]
+      row = [[2 + summand, 2], [2 + summand, 4], [2 + summand, 6]]
       row.each do |field|
         count += 1 if player.fields.include?(field)
       end
@@ -98,30 +99,26 @@ module TicTacToe
       return true if count == 3
     end
 
-    def play_round(player)
-      print_board
-      puts "#{player.mark} Enter the field you want to mark (row, column)."
-      pick = gets.chomp
-      pick = pick.split(',')
-      pick[0] = pick[0].to_i
-      pick[1] = pick[1].to_i
-      set_mark(player, pick)
-      print_board
-      return 'Gameover' if gameover?(player)
+    def board_full?
+      return true if @playable_fields.empty?
+      return false unless @playable_fields.empty?
     end
 
     def start_game
-      player_one = Player.new
-      player_two = Player.new
-      current_player = player_one
+      puts 'Do you wanna play against a human (0), a bot (1), or watch 2 bots (2)?'
+      players=Player.create_players(gets.chomp)
+      current_player = players[0]
       create_board
-      until play_round(current_player) == 'Gameover'
-        if current_player == player_one then current_player = player_two
+      print_board
+      p players
+      until current_player.play_round(self) == 'Gameover'
+        if current_player == players[0] then current_player = players[1]
         else
-          current_player = player_one
+          current_player = players[0]
         end
       end
-      puts "#{current_player.mark} you won the Game!"
+      puts "#{current_player.mark} you won the Game!" unless board_full?
+      puts 'Its a Draw!' if board_full?
     end
   end
 
@@ -134,9 +131,65 @@ module TicTacToe
       select_mark
     end
 
+    def self.create_players(amount_of_bots)
+      if amount_of_bots.to_i == 0
+        player_one = Player.new
+        player_two = Player.new
+      elsif amount_of_bots.to_i == 1
+        player_one = Player.new
+        player_two = ComputerPlayer.new
+      elsif amount_of_bots.to_i == 2
+        player_one = ComputerPlayer.new
+        player_two = ComputerPlayer.new
+      else
+        puts 'try again:'
+        return self.create_players(gets.chomp)
+      end
+      [player_one, player_two]
+    end
+
     def select_mark
       puts 'Select your mark!'
       @mark = gets.chomp.chr
+      puts ''
+    end
+
+    def play_round(game)
+      puts "#{@mark} Enter the field you want to mark (row, column)."
+      game.set_mark(self, string_into_int_array(gets.chomp))
+      puts ''
+      game.print_board
+      return 'Gameover' if game.gameover?(self)
+    end
+
+    def string_into_int_array(string)
+      string = string.split(',')
+      string[0] = string[0].to_i * 2
+      string[1] = string[1].to_i * 2
+      string
+    end
+  end
+
+  class ComputerPlayer < Player
+    def select_mark
+      puts 'Select Computers mark!'
+      @mark = gets.chomp.chr
+      puts ''
+    end
+
+    def play_round(game)
+      game.set_mark(self, game.playable_fields.sample)
+      puts "#{@mark}'s turn:"
+      puts ''
+      game.print_board
+      sleep 1.5
+      return 'Gameover' if game.gameover?(self)
+    end
+  end
+
+  class GameError
+    def self.field_error
+      puts 'field is not playable, try again.'
     end
   end
 end
